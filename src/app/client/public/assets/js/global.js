727069d59babd65f5d24ce61c7ca32b42b8291f6
DylanoBoellaard <336751@student.mboutrecht.nl>
2023-05-15 12:37:23 +0200

    Added comments + finishing touches for cleaner code

--- src/app/client/public/assets/js/global.js
+++ src/app/client/public/assets/js/global.js
@@ -1,4 +1,4 @@
-// Adds the class 'current' to the active page the user is on, which then is used in CSS for styling
+// Adds the class 'current' to the active page the user is on, which then is used in CSS for styling in global.scss
 var path = window.location.pathname;
 var page = path.split("/").pop();
 console.log(page);
