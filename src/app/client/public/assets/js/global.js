8320df49466fac3ab01eacad3152e30d44166e01
DylanoBoellaard <336751@student.mboutrecht.nl>
2023-05-23 10:13:08 +0200

    Fixed bug where homepage didn't get current class for styling when user was on page

--- src/app/client/public/assets/js/global.js
+++ src/app/client/public/assets/js/global.js
@@ -1,7 +1,8 @@
 // Adds the class 'current' to the active page the user is on, which then is used in CSS for styling in global.scss
 var path = window.location.pathname;
+//console.log("Pathname: " + path);     // For troubleshooting
 var page = path.split("/").pop();
-console.log(page);
+//console.log("Pagename: " + page);     // For troubleshooting
 var links = document.querySelectorAll('nav ul li a');
 
 for (var i = 0; i < links.length; i++) {
